b221743f594bcaacc0e3d17e39247e47ce69f510
Luis Rodriguez <zacha@tutanota.com>
2018-04-07 01:46:24 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,17 @@
+class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_song(post)
+    @posts << post
+    post.author = self
+  end
+
+  def posts
+    @posts
+  end
+end

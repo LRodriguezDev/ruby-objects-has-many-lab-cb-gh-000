a77c943e7e253bf808b9ebda44d0054248382a54
Luis Rodriguez <zacha@tutanota.com>
2018-04-07 01:53:16 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,15 @@
 class Author
   attr_accessor :name
 
+  @@post_count
+
   def initialize(name)
     @name = name
     @posts = []
   end
 
   def add_post(post)
+    @@post_count += 1
     @posts << post
     post.author = self
   end
@@ -16,6 +19,7 @@ class Author
   end
 
   def add_post_by_title(post_title)
+    @@post_count += 1
     post = Post.new(post_title)
     @posts << post
     post.author = self
